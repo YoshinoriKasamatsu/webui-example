0a78c5a21f04fb6a3f10db19ebfe508582a44648
Yoshinori Kasamatsu <ka.sa.ma.123@gmail.com>
2024-01-06 18:25:43 +0900

    Server WebAPI追加

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,63 @@
 use actix_files::Files;
-use actix_web::{middleware::Logger, App, HttpServer};
+use actix_web::{middleware::Logger, App, HttpServer, web, HttpResponse, http::header::ContentType,};
+
+mod data_store;
+mod repositories;
+
+async fn get_categories() -> HttpResponse {
+
+    let categories = repositories::get_categories().await;
+    let json_text = serde_json::to_string(&categories).unwrap();
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(json_text)
+}
+
+async fn get_fields() -> HttpResponse {
+
+    let values = repositories::get_fields().await;
+    let json_text = serde_json::to_string(&values).unwrap();
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(json_text)
+}
+
+async fn get_classification() -> HttpResponse {
+
+    let values = repositories::get_classification().await;
+    let json_text = serde_json::to_string(&values).unwrap();
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(json_text)
+}
+
+async fn get_states() -> HttpResponse {
+
+    let values = repositories::get_states().await;
+    let json_text = serde_json::to_string(&values).unwrap();
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(json_text)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+
+
+    data_store::load_work_items_all().await;
+
 
     log::info!("starting HTTP server at http://localhost:8080");
 
     HttpServer::new(|| {
         App::new()
             // .service(Files::new("/images", "static/images/").show_files_listing())
+            .route("api/categories", web::get().to(get_categories))
+            .route("api/fields", web::get().to(get_fields))
+            .route("api/classification", web::get().to(get_classification))
+            .route("api/states", web::get().to(get_states))
             .service(Files::new("/", "./dist/web-frontend/browser").index_file("index.html"))
             .wrap(Logger::default())
     })
---
+++ src/repositories.rs
@@ -0,0 +1,51 @@
+use serde_json::Value;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
+
+pub async fn get_categories() -> Value {
+
+    let categories_path = format!("{}/{}", "./../data", "meta_data");
+    let categories_file_path = format!("{}/{}", &categories_path, "categories.json");
+
+    let mut file = File::open(categories_file_path).await.unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).await.unwrap();
+    let value: Value = serde_json::from_str(&contents).unwrap();
+    value.clone()
+}
+
+pub async fn get_fields() -> Value {
+
+    let path = format!("{}/{}", "./../data", "meta_data");
+    let file_path = format!("{}/{}", &path, "fields.json");
+
+    let mut file = File::open(file_path).await.unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).await.unwrap();
+    let value: Value = serde_json::from_str(&contents).unwrap();
+    value.clone()
+}
+
+pub async fn get_classification() -> Value {
+
+    let path = format!("{}/{}", "./../data", "meta_data");
+    let file_path = format!("{}/{}", &path, "work_item_classification_nodes.json");
+
+    let mut file = File::open(file_path).await.unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).await.unwrap();
+    let value: Value = serde_json::from_str(&contents).unwrap();
+    value.clone()
+}
+
+pub async fn get_states() -> Value {
+
+    let path = format!("{}/{}", "./../data", "meta_data");
+    let file_path = format!("{}/{}", &path, "work_item_states.json");
+
+    let mut file = File::open(file_path).await.unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).await.unwrap();
+    let value: Value = serde_json::from_str(&contents).unwrap();
+    value.clone()
+}
